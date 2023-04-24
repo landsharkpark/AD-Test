8db44787253e5b1c2fbd38b4aac4c3461151d949
LeSean Bruneau <21128666+landsharkpark@users.noreply.github.com>
2023-04-24 18:20:27 -0500

    KD

---
+++ kd-links/kd-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kd-links',
+  templateUrl: './kd-links.component.html',
+  styleUrls: ['./kd-links.component.css']
+})
+export class KdLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
